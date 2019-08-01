6de3d1d24aebca7a6a99c336b6d4ce1744bfe4f9
JunyaHB <a1933jh@aiit.ac.jp>
2019-08-01 17:31:09 +0900

    second

---
+++ src/app/todo/todo.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
+import { TodoRoutingModule } from './todo-routing.module';
+import { TodoComponent } from './todo/todo.component';
+
+/**
+ * Todo機能をもつモジュールです
+ */
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CollapseModule.forRoot(),
+    TodoRoutingModule
+  ],
+  declarations: [TodoComponent]
+})
+export class TodoModule { }
